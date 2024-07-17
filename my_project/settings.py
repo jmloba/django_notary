1560b9520c3d0e7ad15672e4c0472f7ed514a105
jovenloba <117356497+jovenloba@users.noreply.github.com>
2024-07-18 00:18:57 +0800

    update5

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,11 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  config('SECRET_KEY')
+# SECRET_KEY =  config('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # for online purposes
@@ -122,7 +123,11 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
     },
-   
+}
+
+DATABASES={
+  "default":dj_database_url.parse(os.environ.get("DATABASE_URL"))
+  
 }
 
 # database_url = os.environ.get('DATABASE_URL')
